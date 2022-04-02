6c02684b305154d611041f3749eeebd9824b2b0f
Everton Pek <45766342+pektronaso@users.noreply.github.com>
2022-04-02 14:09:48 -0300

    last updates

--- assets/js/ajax-enter.js
+++ assets/js/ajax-enter.js
@@ -1,6 +1,7 @@
-
 /*ajax-mail.js*/
 $(function() {
+
+	
 
 	// Get the form.
 	var form = $('#enter-form');
@@ -18,8 +19,6 @@ $(function() {
 		e.preventDefault();
 		
 		
-			
-
 		// Serialize the form data.
 		var formData = $(form).serializeArray();
 
@@ -39,22 +38,35 @@ $(function() {
 			$(formMessages).removeClass('error');
 			$(formMessages).addClass('success');
 
-			// Set the message text.
-			$(formMessages).text(response);
-			
+			// Verify callback and Set the message text.			
+			if (response[0] == "rejected") {
+				$(formMessages).text(response[1]);
+				console.log(response);
+			} else {
+				$(formMessages).text(response[0]);
+				console.log(response);
+			}
 
 			// Clear the form.
-			$('#register-form input,#register-form textarea').val('');
+			$('#email, textarea').val('');
+			$('#senha, textarea').val('');
+			$('#imgLoad').hide();
+			
 		})
 		.fail(function(data) {
+
+			
+
 			// Make sure that the formMessages div has the 'error' class.
 			$(formMessages).removeClass('success');
 			$(formMessages).addClass('error');
 
 			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
+			if (data.responseText !== '') {				
+				$('#imgLoad').hide();
+				$(formMessages).text(data.statusText);
 			} else {
+				console.log("point 2");
 				$(formMessages).text('Oops! An error occured and your message could not be sent.');
 			}
 		});
